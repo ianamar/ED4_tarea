43d6e72faf2fab7397d31c7f44c1ed0aca308dd2
Iana <Iana@DESKTOP-H4I1J3V>
2022-02-05 19:26:32 +0100

    Cambios en la tarea

--- src/main/java/CCuenta.java
+++ src/main/java/CCuenta.java
@@ -1,19 +1,90 @@
 /**
  *
- * @author Iana
+ * @author Iana Markarova
  */
 public class CCuenta {
 
-
+    /**
+     * atributos de la clase CCuenta
+     */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
+    
+    //encapsulando los cuatro atributos de la clase CCuenta, creando los
+    //getters y setters correspondientes
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
 
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+    /**
+     * Un constructor sin parametros para la clase CCuenta
+     */
     public CCuenta()
     {
     }
-
+    
+    /**
+     * Un constructor con parametros para la clase CCuenta
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -21,24 +92,40 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Un método que devuelve el saldo de la cuenta
+     * @return 
+     */
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
+    /**
+     * Un método para hacer una ingreso en la cuenta. Primero se comprueba
+     * que la cantidad no sea negativa
+     * @param cantidad
+     * @throws Exception 
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Un método para hacer un retirada de la cuenta. Primero se comprueba
+     * que la cantidad no sea negativa y que haya sufieciento saldo en la cuenta
+     * @param cantidad
+     * @throws Exception 
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,35 @@
 /**
  *
- * @author Iana
+ * @author Iana Markarova
  */
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        
+        //inicialización del método operativa_cuenta
+        operativa_cuenta();
+    }
+
+    //creando el método operativa_cuenta que englobe las sentencias 
+    //de la clase Main que operan con el objeto cuenta1
+    
+    public static void operativa_cuenta() {
+        
+        CCuenta cuenta1;
         double saldoActual;
-
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
+        float cantidad; //Añadiendo un nuevo parámetro de nombre cantidad y de tipo float
+        
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
-
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(695);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
